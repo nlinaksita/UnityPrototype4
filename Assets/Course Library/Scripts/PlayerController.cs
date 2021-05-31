20acf3d8eb1f3e0de105ed5853c7f8785d4c260a
Nicole L <linaksitan@gmail.com>
2021-05-31 13:41:15 -0700

    Added player lives system

--- Assets/Course Library/Scripts/PlayerController.cs
+++ Assets/Course Library/Scripts/PlayerController.cs
@@ -1,13 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
     public float speed;
     public bool hasPowerup;
     public GameObject powerupIndicator;
+    public Text playerLivesText;
 
+    private int playerLives;
     private Rigidbody playerRb;
     private GameObject focalPoint;
     private float powerupStrength = 15.0f;
@@ -16,6 +19,9 @@ public class PlayerController : MonoBehaviour
     {
         playerRb = GetComponent<Rigidbody>();
         focalPoint = GameObject.Find("Focal Point");
+
+        // Start off with 3 lives
+        playerLives = 3;
     }
 
     // Update is called once per frame
@@ -26,6 +32,31 @@ public class PlayerController : MonoBehaviour
         playerRb.AddForce(focalPoint.transform.forward * speed * forwardInput);
         playerRb.AddForce(focalPoint.transform.right * speed * horizontalInput);
         powerupIndicator.transform.position = transform.position + new Vector3(0, 1f, 0);
+
+        // If the player falls off the stage
+        if (gameObject.transform.position.y < -10)
+        {
+            // Reduce the number of lives left
+            playerLives--;
+
+            // If there are no lives left and the player falls off the stage, gameover
+            if (playerLives < 0)
+            {
+                GameOver();
+            } else {
+                playerLivesText.text = "x " + playerLives;
+
+                // If the player has a powerup and falls, remove the powerup indicator
+                hasPowerup = false;
+                powerupIndicator.gameObject.SetActive(false);
+
+                // Remove any previous force on the object to prevent it from moving when it respawns
+                playerRb.velocity = Vector3.zero;
+                playerRb.angularVelocity = Vector3.zero;
+                // Reset the player position
+                gameObject.transform.position = Vector3.zero;
+            }
+        }
     }
 
     private void OnTriggerEnter(Collider other)
@@ -58,5 +89,11 @@ public class PlayerController : MonoBehaviour
 
             enemyRigidbody.AddForce(awayFromPlayer * powerupStrength, ForceMode.Impulse);
         }
+    }
+
+    public void GameOver()
+    {
+        Time.timeScale = 0;
+        //Debug.Log("Game Over");
     }
 }
