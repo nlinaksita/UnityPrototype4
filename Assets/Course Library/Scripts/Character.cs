92d20341fd6b9f86d41b4f2ee419035bb00f985c
Nicole L <linaksitan@gmail.com>
2021-06-03 17:00:47 -0700

    Refactored EnemyStomper to inherit from Character. Moved stomping code to Character

--- Assets/Course Library/Scripts/Character.cs
+++ Assets/Course Library/Scripts/Character.cs
@@ -5,16 +5,17 @@ using UnityEngine;
 public class Character : MonoBehaviour
 {
     public GameObject rocket;
+    public const int TYPE_PLAYER = 0;
+    public const int TYPE_ENEMY = 1;
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     // rockets: Number of rockets
@@ -26,6 +27,77 @@ public class Character : MonoBehaviour
             // Evenly distribute rockets around the player relative to a random rotation
             float rotation = (360 / (float)rockets) * i + relativeAngle;
             Instantiate(rocket, gameObject.transform.position, Quaternion.Euler(0, rotation, 0));
+        }
+    }
+
+    // Stomping methods
+    // rb = rigidbody, jp = jumping power, sp = stomping power, ps = powerup strength,
+    // sr = stomp range, type = 0: rb is player, type = 1: rb is enemy
+    public void StompUp(Rigidbody rb, float jp, float sp, float ps, float sr, int type)
+    {
+        // Stop velocity
+        rb.velocity = Vector3.zero;
+        rb.angularVelocity = Vector3.zero;
+
+        // Reset rotation
+        transform.rotation.eulerAngles.Set(0, 0, 0);
+
+        // Apply upward force
+        rb.AddForce(Vector3.up * jp, ForceMode.Impulse);
+
+        StartCoroutine(StompDown(rb, sp, ps, sr, type));
+    }
+
+    private IEnumerator StompDown(Rigidbody rb, float sp, float ps, float sr, int type)
+    {
+        yield return new WaitForSeconds(0.3f);
+        // Apply downward force
+        rb.AddForce(Vector3.down * sp, ForceMode.Impulse);
+
+        StartCoroutine(StompPushEnemies(rb, ps, sr, type));
+    }
+
+    private IEnumerator StompPushEnemies(Rigidbody rb, float ps, float sr, int type)
+    {
+        yield return new WaitForSeconds(0.2f);
+
+        switch (type)
+        {
+            case TYPE_PLAYER:
+                Enemy[] enemies = FindObjectsOfType<Enemy>();
+                for (int i = 0; i < enemies.Length; i++)
+                {
+                    StompPushEnemies(enemies[i].gameObject.GetComponent<Rigidbody>(),
+                        rb,
+                        sr,
+                        ps);
+                }
+                break;
+            case TYPE_ENEMY:
+                PlayerController p = FindObjectOfType<PlayerController>();
+                StompPushEnemies(p.gameObject.GetComponent<Rigidbody>(),
+                    rb,
+                    sr,
+                    ps);
+                break;
+        }
+    }
+
+
+    private void StompPushEnemies(Rigidbody enemy, Rigidbody self, float sr, float ps)
+    {
+        Vector3 awayFromSelf = (enemy.transform.position - self.transform.position);
+        // calculate the distance from the enemy to the player
+        float distance = Mathf.Abs((awayFromSelf).magnitude);
+        //Debug.Log(distance);
+        if (distance <= sr)
+        {
+            // apply force to enemy (enemies further away will be affected less than those closer)
+            float stompMagPower = Mathf.Abs(distance - sr) / sr;
+            //Debug.Log(stompMagPower * powerupStrength);
+
+            enemy.AddForce(awayFromSelf * ps
+                * stompMagPower, ForceMode.Impulse);
         }
     }
 }
--- Assets/Course Library/Scripts/EnemyStomper.cs
+++ Assets/Course Library/Scripts/EnemyStomper.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyStomper : MonoBehaviour
+public class EnemyStomper : Character
 {
     private bool isStomping = false;
     private Rigidbody enemyRb;
@@ -29,56 +29,13 @@ public class EnemyStomper : MonoBehaviour
         yield return new WaitForSeconds(stompRate);
         // Set flag so that cannot interrupt stomp with another stomp
         isStomping = true;
-
-        // Stop velocity
-        enemyRb.velocity = Vector3.zero;
-        enemyRb.angularVelocity = Vector3.zero;
-
-        // Reset rotation
-        transform.rotation.eulerAngles.Set(0, 0, 0);
-
-        // Apply upward force
-        enemyRb.AddForce(Vector3.up * jumpPower, ForceMode.Impulse);
-
-        StartCoroutine(StompDown());
-    }
-
-    // Player stomps down after short delay
-    private IEnumerator StompDown()
-    {
-        yield return new WaitForSeconds(0.3f);
-        // Apply downward force
-        enemyRb.AddForce(Vector3.down * stompPower, ForceMode.Impulse);
-
-        StartCoroutine(StompPushEnemies());
-    }
-
-    // Stomp applies force to enemies if they are within range
-    private IEnumerator StompPushEnemies()
-    {
-        yield return new WaitForSeconds(0.2f);
-
-        // Iterate over enemies and check if they are within stompRange
-        PlayerController player = FindObjectOfType<PlayerController>();
-        
-        Vector3 awayFromEnemy = (player.transform.position - transform.position);
-        // calculate the distance from the enemy to the player
-        float distance = Mathf.Abs((awayFromEnemy).magnitude);
-        //Debug.Log(distance);
-        if (distance <= stompRange)
-        {
-            // apply force to enemy (enemies further away will be affected less than those closer)
-            float stompMagPower = Mathf.Abs(distance - stompRange) / stompRange;
-            //Debug.Log(stompMagPower * powerupStrength);
-
-            player.GetComponent<Rigidbody>().AddForce(awayFromEnemy * powerupStrength
-                * stompMagPower, ForceMode.Impulse);
-        }
-        
-
-        // reset stomping flag
-        isStomping = false;
-
+        StompUp(enemyRb,
+            jumpPower,
+            stompPower,
+            powerupStrength,
+            stompRange,
+            TYPE_ENEMY
+            );
         StartCoroutine(StompUp());
     }
 }
