5be2522ad6d9886584a4725a2c81d4a8ef4c4084
Nicole L <linaksitan@gmail.com>
2021-06-05 16:35:58 -0700

    added error message and check for wrong spelling of audioclip name

--- Assets/Course Library/Scripts/AudioManager.cs
+++ Assets/Course Library/Scripts/AudioManager.cs
@@ -21,6 +21,9 @@ public class AudioManager : MonoBehaviour
     public void Play(string name)
     {
         Sound s = Array.Find(sounds, sound => sound.name == name);
+        if (s == null)
+            Debug.Log("Error: Could not find sound effect with name '" + name + "'");
+            return;
         s.source.Play();
     }
 }
